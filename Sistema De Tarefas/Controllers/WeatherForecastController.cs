7079c1bae1c9d3f2c39f61a78ffcbb85370d9454
Daniel <d.loureiro@aluno.senai.br>
2024-05-30 15:34:32 -0300

    Interface

--- Sistema De Tarefas/Controllers/WeatherForecastController.cs
+++ Sistema De Tarefas/Controllers/WeatherForecastController.cs
@@ -9,7 +9,7 @@ namespace Sistema_De_Tarefas.Controllers
         private static readonly string[] Summaries = new[]
         {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+        };
 
         private readonly ILogger<WeatherForecastController> _logger;
 
@@ -19,15 +19,6 @@ namespace Sistema_De_Tarefas.Controllers
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
+
     }
 }
